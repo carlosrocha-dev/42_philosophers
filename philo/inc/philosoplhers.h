a9751be6fe09891845c1d0befd132f1810f7a06b
Carlos Rocha <carlos.rocha01@gmail.com>
2023-08-22 23:34:35 -0300

    feat: including validation input

---
+++ philo/inc/philosoplhers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosoplhers.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 23:30:35 by caalbert          #+#    #+#             */
+/*   Updated: 2023/08/22 23:31:03 by caalbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <semaphore.h>
+# include <stdio.h>
+
+#endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,19 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 22:13:58 by caalbert          #+#    #+#             */
-/*   Updated: 2023/08/22 22:14:47 by caalbert         ###   ########.fr       */
+/*   Updated: 2023/08/22 23:32:20 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "inc/philosoplhers.h"
+
 int	main(int ac, char **av)
 {
+	(void)av;
 	if (ac < 5 || ac > 6)
+	{
+		printf("Error: wrong number of arguments\n");
 		return (1);
+	}
 	return (0);
 }
