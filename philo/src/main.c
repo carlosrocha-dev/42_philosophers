6fdc96f3f530cb3cbf5ae4c9c3f4764d43fa7b88
Carlos Rocha <carlos.rocha01@gmail.com>
2023-08-24 09:38:15 -0300

    feat(args analyser)

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 22:13:58 by caalbert          #+#    #+#             */
-/*   Updated: 2023/08/23 12:27:55 by caalbert         ###   ########.fr       */
+/*   Updated: 2023/08/24 09:34:08 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,26 @@ int	args_analyser(int ac, char **av)
 	int	j;
 
 	i = 1;
+	j = 0;
 	while (i < ac)
 	{
 		j = 0;
 		while (av[i][j])
 		{
-			if (!ft_isdigit(av[i][j]))
-			{
-				printf("Error: wrong argument\n");
-				return (1);
-			}
+			if (av[i][j] < 48 || av[i][j] > 57)
+				return (0);
 			j++;
 		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	t_args	*args;
 
+	printf("Init Philo:\n");
 	if (ac < 5 || ac > 6)
 	{
 		printf("Error: wrong number of arguments\n");
@@ -46,8 +45,17 @@ int	main(int ac, char **av)
 	}
 	if (!args_analyser(ac, av))
 		return (1);
+	printf("Pass analyser:\n");
+	printf("Args_analyser: %d\n\n\n", args_analyser(ac, av));
 	args = parse_args(ac, av);
 	if (!args)
 		return (1);
+	printf("Pass args validator:\n");
+	printf("Number of philosophers: %d\n", args->num_philo);
+	printf("Time to die: %d\n", args->time_to_die);
+	printf("Time to eat: %d\n", args->time_to_eat);
+	printf("Time to sleep: %d\n", args->time_to_sleep);
+	printf("Number of meals: %d\n", args->num_meals);
+	printf("\n");
 	return (0);
 }
