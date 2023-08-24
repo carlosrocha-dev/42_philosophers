55df11acb4979f81c264960d7acf7aa9ff57687f
Carlos Rocha <carlos.rocha01@gmail.com>
2023-08-24 19:34:26 -0300

    chore(headers): split hedears in two files

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 23:30:35 by caalbert          #+#    #+#             */
-/*   Updated: 2023/08/24 18:23:08 by caalbert         ###   ########.fr       */
+/*   Updated: 2023/08/24 19:31:45 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/time.h>
-
-typedef struct s_args	t_args;
-typedef struct s_philo
-{
-	int				id;
-	int				left_fork;
-	int				right_fork;
-	long long		last_meal;
-	int				is_done;
-	int				time_to_ate;
-	t_args			*args;
-	pthread_t		thread;
-	pthread_mutex_t	is_done_mutex;
-}	t_philo;
-
-typedef struct s_args
-{
-	int				num_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				num_meals;
-	int				is_dead;
-	int				stop_simulation;
-	int				meals_finished;
-	long long		start_time;
-	t_philo			*philo;
-	pthread_mutex_t	write_mutex;
-	pthread_mutex_t	last_meal_mutex;
-	pthread_mutex_t	death_mutex;
-	pthread_mutex_t	*forks;
-}	t_args;
 
 int			ft_isdigit(int c);
 void		*parse_args(int argc, char **argv);
---
+++ philo/inc/structs.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 19:30:21 by caalbert          #+#    #+#             */
+/*   Updated: 2023/08/24 19:32:18 by caalbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+# include "philosophers.h"
+
+typedef struct s_args	t_args;
+typedef struct s_philo
+{
+	int				id;
+	int				left_fork;
+	int				right_fork;
+	long long		last_meal;
+	int				is_done;
+	int				time_to_ate;
+	t_args			*args;
+	pthread_t		thread;
+	pthread_mutex_t	is_done_mutex;
+}	t_philo;
+
+typedef struct s_args
+{
+	int				num_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_meals;
+	int				is_dead;
+	int				stop_simulation;
+	int				meals_finished;
+	long long		start_time;
+	t_philo			*philo;
+	pthread_mutex_t	write_mutex;
+	pthread_mutex_t	last_meal_mutex;
+	pthread_mutex_t	death_mutex;
+	pthread_mutex_t	*forks;
+}	t_args;
